bb097985e45be475f6ecec3e4a128a3b4e8149a5
Bashademy <bashademy@gmail.com>
2024-02-27 18:08:59 +0530

    added hover boxes in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,41 @@
+import { useState } from "react"
 
 const Home = () => {
 
-    const myStyle = {
-        height: 400,
-        backgroundColor: "#f06156",
-        borderRadius: 10
+    const [isHovered1, setIsHovered1] = useState(false);
+    const [isHovered2, setIsHovered2] = useState(false);
+
+    // const handleMouseEnter = () => setIsHovered(true);
+    // const handleMouseLeave = () => setIsHovered(false);
+
+  const styles = {
+    
+  };
+
+    const liveBox = {
+        height: 300,
+        backgroundColor: isHovered2 ? '#d1cdcd' : "#f0f4f9",
+        borderRadius: 12,
+        justifyContent: "center" 
         }
+
+    const tubeBox = {
+        height: 300,
+        borderRadius: 12,
+        justifyContent: "center", 
+        backgroundColor: isHovered1 ? '#d1cdcd' : "#f0f4f9",
+
+    }
 
 
     return(
         <div className="container">
             <div className="row">
-                <div className="border border-danger col-md mx-md-3" style={myStyle}>
-                This box has a thick, red, das rounded corners.
+                <div className="col-md mx-md-3" style={tubeBox} onMouseEnter={() => setIsHovered1(true)} onMouseLeave={() => setIsHovered1(false)}>
+                <h6 style={{fontSize: 50}}>YouTube Channel</h6>
                 </div>
-                <div className="border border-danger col-md mx-md-3" style={myStyle}>
-                This box has a thick, red, dashed border with rounded corners.
+                <div className="col-md mx-md-3" style={liveBox} onMouseEnter={() => setIsHovered2(true)} onMouseLeave={() => setIsHovered2(false)}>
+                <h6 style={{fontSize: 50}}>Live TV</h6>
                 </div>
             </div>
         </div>
